45e9a88217da9ce17a151e79e9aa8b703ac8e1b9
Pawel <pawelurbanik94@gmail.com>
2021-03-13 08:20:08 +0100

    Race Condition

---
+++ src/pl/pawel/racecondition/Race.java
@@ -0,0 +1,36 @@
+package pl.pawel.racecondition;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Race {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Counter counter = new Counter();
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(()-> counter.incerase());
+        }
+
+        executorService.awaitTermination(7, TimeUnit.SECONDS);
+        executorService.shutdown();
+
+        System.out.println(counter.getCount());
+    }
+
+
+}
+
+class Counter{
+    private int count;
+
+    public void incerase(){
+        count = count +1;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
